afee20cdcf38ae2dcdf0322f4cccc541e176993e
mostlytoast <tom-simon@live.com>
2025-06-10 13:14:34 -0500

    try adding tests

---
+++ computer_code/api/test_index.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import patch, MagicMock
+# import index
+
+
+import sys
+import unittest
+from unittest.mock import patch, MagicMock
+import numpy as np
+import index
